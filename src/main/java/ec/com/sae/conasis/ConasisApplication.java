15563382ae0735ef409c736c95c00131c310a241
ronyreyna1995 <ronyreyna1995@gmail.com>
2020-06-06 23:53:38 -0500

    listo

---
+++ src/main/java/ec/com/sae/conasis/ConasisApplication.java
@@ -0,0 +1,13 @@
+package ec.com.sae.conasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConasisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConasisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ec/com/sae/conasis/ConasisApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.sae.conasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConasisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
